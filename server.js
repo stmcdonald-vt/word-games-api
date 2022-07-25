89653ca040faa01d80fa4159f3a2fdb000a0813d
Sean <stmcdonald@vt.edu>
2022-07-25 15:48:55 -0400

    implemented serverless framework

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const serverless = require('serverless-http');
 const express = require("express");
 const app = express();
 
 const gameRouter = require("./routes/game/game");
 app.use("/game", gameRouter);
 
+module.exports.handler = serverless(app);
 module.exports = app.listen(3000);

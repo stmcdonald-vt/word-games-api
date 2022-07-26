7de0c3293e97845f51bcf465beaba233a82b47b2
Sean <stmcdonald@vt.edu>
2022-07-26 14:29:54 -0400

    enable CORS on gameRouter

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const serverless = require('serverless-http');
 const express = require("express");
 const app = express();
+const cors = require("cors")
 
 const gameRouter = require("./routes/game/game");
+gameRouter.use(cors());
 app.use("/game", gameRouter);
 app.use(express.json());
 
